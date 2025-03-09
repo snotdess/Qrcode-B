29023956bfbc237b0ed66035895a0b75f8a65307
mideyolu <olumideoluwuyi91@gmail.com>
2025-03-09 19:50:47 +0100

    Updates

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False
 # Function to initialize the database
 async def init_db():
     async with engine.begin() as conn:
-        #await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
 
 # Dependency to get the database session
